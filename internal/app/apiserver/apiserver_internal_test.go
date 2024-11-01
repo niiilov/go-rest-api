8ec59773ee05904905d3dbf2ca2b8bb4f9d88de3
niiilov <nilovaslanbek2006@gmail.com>
2024-11-01 22:07:19 +0300

    server

--- internal/app/apiserver/apiserver_internal_test.go
+++ internal/app/apiserver/apiserver_internal_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func testAPIServer_HandleHello(t *testing.T) {
+func TestAPIServer_HandleHello(t *testing.T) {
 	s := New(NewConfig())
 	rec := httptest.NewRecorder()
 	req, _ := http.NewRequest(http.MethodGet, "/hello", nil)
